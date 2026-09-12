62de34bdffc994a5d26db8634b0b0c6f85101d0a
IMRAN AHMED <project.developer.tech@gmail.com>
2026-09-12 16:20:34 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* সার্ভিস ওয়ার্কার — অ্যাপ ইনস্টল, অফলাইন সাপোর্ট ও ক্যাশিং হ্যান্ডেল করে */
-const CACHE_VERSION = 'rjf-v1.03.13';
+const CACHE_VERSION = 'rjf-v1.03.14';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const RUNTIME_CACHE = CACHE_VERSION + '-runtime';
 

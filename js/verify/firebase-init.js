6fa4e10d6f195b6f5c389342877cd91ea9fd4f66
IMRAN AHMED <project.developer.tech@gmail.com>
2026-09-25 22:33:11 +0600

    Update firebase-init.js

--- js/verify/firebase-init.js
+++ js/verify/firebase-init.js
@@ -2,14 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC-ke7FIUPX5Ksow8vJQ4axmGAIdiKd49Q",
-    authDomain: "member-selection.firebaseapp.com",
-    projectId: "member-selection",
-    storageBucket: "member-selection.firebasestorage.app",
-    messagingSenderId: "434008909239",
-    appId: "1:434008909239:web:a790d1e0603ebfdbd27432",
-    measurementId: "G-JVMKJZLCC5"
+  apiKey: "AIzaSyBMfeFWtyE-raexNO8DkpyXBQFvE3yNIRU",
+  authDomain: "rupshajf.firebaseapp.com",
+  projectId: "rupshajf",
+  storageBucket: "rupshajf.firebasestorage.app",
+  messagingSenderId: "878760730320",
+  appId: "1:878760730320:web:39ef84c2b447e24df5c8d5",
+  measurementId: "G-BF5ZPK7NZS"
 };
 
 const app = initializeApp(firebaseConfig);

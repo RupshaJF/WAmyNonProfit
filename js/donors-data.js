34286e8e6284341705e9ac909496e34488aaca52
IMRAN AHMED <project.developer.tech@gmail.com>
2026-09-25 22:58:08 +0600

    Update Firebase configuration in donors-data.js

--- js/donors-data.js
+++ js/donors-data.js
@@ -50,7 +50,7 @@ RJF.refreshDonorListFromFirestore = function () {
   ]).then(function (mods) {
     var appMod = mods[0];
     var fsMod = mods[1];
-    var cfg = { apiKey: "AIzaSyC-ke7FIUPX5Ksow8vJQ4axmGAIdiKd49Q", authDomain: "member-selection.firebaseapp.com", projectId: "member-selection", storageBucket: "member-selection.firebasestorage.app", messagingSenderId: "434008909239", appId: "1:434008909239:web:a790d1e0603ebfdbd27432" };
+    var cfg = { apiKey: "AIzaSyBMfeFWtyE-raexNO8DkpyXBQFvE3yNIRU", authDomain: "rupshajf.firebaseapp.com", projectId: "rupshajf", storageBucket: "rupshajf.firebasestorage.app", messagingSenderId: "878760730320", appId: "1:878760730320:web:39ef84c2b447e24df5c8d5", measurementId: "G-BF5ZPK7NZS" };
     var app = appMod.getApps().length ? appMod.getApp() : appMod.initializeApp(cfg);
     var db = fsMod.getFirestore(app);
     var q = fsMod.query(fsMod.collection(db, 'donors'), fsMod.orderBy('id', 'asc'));
